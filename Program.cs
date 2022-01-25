260700e2712587390a0b9f464c10f575df2b6d6c
Dmitry Anoschenkov <danoschenkov@gmail.com>
2022-01-25 12:37:10 +0300

    First draft prepared

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//  Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых
+// меньше или равна 3. 
+
+string[] myArray = new string {"monkey", "bug", "bee", "lion", "cat", "dog", "111", "!2&*^"};
+
+string PickItemsFromArray = string [ExternalArray]
+{
+    string[] ArrayResult = new string[ExternalArray.GetLength];
+        for (new int i = 0; i < ExternalArray.GetLength; i++)
+            {
+                if (ExternalArray[i].Length < 3)
+                    ArrayResult[i] = ExternalArray[i];
+                else
+                    Array.Resize(ref ArrayResult, ArrayResult.Length - 1);
+            Console.Write($"{ArrayResult(i)} + " " ");
+            }
+}
+
+PickItemsFromArray
