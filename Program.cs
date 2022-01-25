cfd0b99753df3fea1c02c5ddd3f409538a7061db
Dmitry Anoschenkov <danoschenkov@gmail.com>
2022-01-25 13:18:12 +0300

    Errors fixed

--- Program.cs
+++ Program.cs
@@ -1,19 +1,38 @@
 ﻿//  Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых
 // меньше или равна 3. 
 
-string[] myArray = new string {"monkey", "bug", "bee", "lion", "cat", "dog", "111", "!2&*^"};
+string[] myArray = new string[] {"monkey", "a", "12", "bee", "lion", "cat", "dog", "111", "!2&*^"};
 
-string PickItemsFromArray = string [ExternalArray]
+void PrintArray(string[] ExternalArray)
 {
-    string[] ArrayResult = new string[ExternalArray.GetLength];
-        for (new int i = 0; i < ExternalArray.GetLength; i++)
-            {
-                if (ExternalArray[i].Length < 3)
-                    ArrayResult[i] = ExternalArray[i];
-                else
-                    Array.Resize(ref ArrayResult, ArrayResult.Length - 1);
-            Console.Write($"{ArrayResult(i)} + " " ");
-            }
+    for(int i = 0; i < ExternalArray.Length; i++)
+        Console.Write(ExternalArray[i] + " ");
+    Console.WriteLine();
 }
 
-PickItemsFromArray
+string[] PickItemsFromArray(string[] ExternalArray)
+{
+    string[] ArrayResult = new string[ExternalArray.Length];
+
+    int counter = 0;
+    for (int i = 0; i < ExternalArray.Length; i++)
+    {
+        if (ExternalArray[i].Length <= 3) 
+        {
+            ArrayResult[counter] = ExternalArray[i];
+            counter = counter + 1;
+        }
+    }
+
+    Array.Resize(ref ArrayResult, counter);
+
+    return ArrayResult;
+}
+
+Console.WriteLine("Original array: ");
+PrintArray(myArray);
+
+string[] ArrayFinal = PickItemsFromArray(myArray);
+
+Console.WriteLine("Array with items with less then 3 symbols is:");
+PrintArray(ArrayFinal);
